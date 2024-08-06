858a977c5852614a86333d3218696fe078154546
Thatweb3Develope <fredixe43@gmail.com>
2024-08-06 13:53:47 +0100

    Made the final changes, modified the UI, updated few functions

--- js/main.js
+++ js/main.js
@@ -1,18 +1,37 @@
+// Get all the input fields 
 const amount = document.getElementById("amount-input");
 const years = document.getElementById("years-input");
 const rate = document.getElementById("rate");
 
+// Get span tags to display the error messages
 const errorMsgAmount = document.getElementById("amountErrorMsg");
 const errorMsgRate = document.getElementById("rateErrorMsg");
 const errorMsgYear = document.getElementById("yearErrorMsg");
 
+const amountSpan = document.getElementById('amount-span');
+const yearSpan = document.getElementById('year-span');
+const rateSpan = document.getElementById('rate-span');
+
+// Gets the input container to display error when inputs are empty
 const amountErrorContent = document.getElementById("input-amount-wrapper");
-const yearErrorContent = document.getElementById("input-year-wrapper");
+const yearErrorContent = document.getElementById("");
 const rateErrorContent = document.getElementById("input-rate-wrapper");
 
-const clearBtn = document.getElementById("clearBtn");
+
+const nextWrapper = document.getElementById('year-wrapper');
+
+
+// The Clear Btn declaration
+const clearBtn = document.getElementById("clearBtn"); 
+
+// The calculate button declaration
 const calculateBtn = document.getElementById("calculate");
 const selectedType = document.getElementsByName("mortage-type");
+
+
+let hasError ;
+// Clear the input field function
+
 
 clearBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -25,55 +44,78 @@ calculateBtn.addEventListener("click", (event) => {
   event.preventDefault();
   let typeOption;
 
-  console.log(amount);
+  // if (years.value === '') {
+  //  '1px solid red';
+  //   nextWrapper.style.zIndex = 100;
+  // }
   // Trim the input values and parse them
   let amountValue = parseFloat(amount.value);
   let yearsValue = parseInt(years.value, 10);
   let rateValue = parseFloat(rate.value);
-
-  let hasError = false;
-
+  // let hasError = false;
+const errorMessage =  "The field is required!";
+const errorBorder = "1px solid red";
 
   if (amount.value === "") {
-    errorMsgAmount.innerHTML = "The field is required!";
-    amountErrorContent.style.border = "1px solid red";
+    errorMsgAmount.innerHTML = errorMessage;
+    amountErrorContent.style.border = errorBorder;
+    amountSpan.style.backgroundColor = 'red';
+    amountSpan.style.color='white';
     hasError = true;
-    console.log("errorAmount");
   }
+
+ 
 
   if (rate.value === "") {
-    errorMsgRate.innerHTML = "The field is required!";
-    rateErrorContent.style.border = "1px solid red";
-    console.log("errorRate");
+    errorMsgRate.innerHTML = errorMessage;
+    rateErrorContent.style.border = errorBorder;
+    rateSpan.style.backgroundColor = 'red';
+    rateSpan.style.color='white';
+    hasError=true;
+  }
+
+ if (years.value === "") {
+    errorMsgYear.innerHTML = errorMessage;
+    nextWrapper.style.border = errorBorder;
+    yearSpan.style.backgroundColor = 'red';
+    yearSpan.style.color='white';
     hasError = true;
   }
 
-  if (years.value === "") {
-    errorMsgYear.innerHTML = "The field is required!";
-    yearErrorContent.style.border = "1px solid red";
-    hasError = true;
-    console.log("errorYear");
-  }
+
+  // check if radio is checked
+
+  let isChecked = false;
+
+  for (let i = 0; i < selectedType.length; i++) {
+    if (selectedType[i].checked) {
+        isChecked = true;
+        break;
+    }
+}
 
 
-//   const repayTypeInput = document.getElementsByName('mortage-type');
+if (!isChecked) {
+  const errorSpan = document.getElementById('repay-type-error');
 
-//  repayTypeInput.forEach(input=>{
+        errorSpan.innerHTML = errorMessage;
+        errorSpan.style.color = 'red';
     
-//    if (input.checked = false ) console.log('select type')
-//  })
+}
 
-  // if (hasError) {
-  //   return;
-  // }
+if (hasError) {
+  return ;
+}
+
   selectedType.forEach((option) => {
+    
     if (option.checked) {
       typeOption = option.value;
-      console.log(1);
-    }else{
-        
-        console.log('select one type')
+      
     }
+    
+    // else{
+    //     
   });
 
   switch (typeOption) {
@@ -88,6 +130,8 @@ calculateBtn.addEventListener("click", (event) => {
   }
 });
 
+
+// Function to display the result after been calculated.
 function displayResult (interestResult, monthlyInterest){
 
     const emptyWrapper = document.getElementById('empty-wrapper');
@@ -106,20 +150,30 @@ function displayResult (interestResult, monthlyInterest){
    
 }
 
+
+// Function to calculate the Interest when selected.
 function getRepaymentAmount(amount, year, rate) {
   let totalRepay;
   totalRepay = amount * year * (rate / 100);
 
   const totalRepayDisplay  = formatNumber(totalRepay.toFixed(2));
+const amountMonthly = year * 12;
 
   
-  const monthlyInterest = totalRepay/12;
+  const monthlyInterest = formatNumber((totalRepay/amountMonthly).toFixed(2));
 
   displayResult( totalRepayDisplay, monthlyInterest);
+
+  amount.value==''
+  years.value =='';
+  rate.value == ''
 
   console.log("Total Repay:", totalRepay);
   return totalRepay;
 }
+
+
+// Function to format the result to add commas 
 
 function formatNumber(number) {
   // Split the number on the decimal point
@@ -129,3 +183,15 @@ function formatNumber(number) {
   // Join the integer and decimal parts
   return parts.join(".");
 }
+
+
+
+
+
+
+
+
+window.onload = function() {
+  var image = document.getElementById('animatedImage');
+  image.classList.add('animate');
+};
